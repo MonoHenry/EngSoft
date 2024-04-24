6477e90449a773cc42ef23a4026a49785d8815ad
MonoHenry <andrepericlesnelli@gmail.com>
2024-04-24 03:19:02 -0300

    SRP explicado

---
+++ Solid/Single/Scerto.cpp
@@ -0,0 +1,23 @@
+class Carro{
+    public:
+        bool ligar();
+
+};
+
+
+class CalcIPVA{
+    public:
+        float calcularIPVA();
+
+};
+
+
+class LigarMecanico{
+    public:
+        bool ligarMecanico();
+
+
+
+};
+
+
